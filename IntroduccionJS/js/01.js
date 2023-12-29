37bd801698dee80bbce1baa18e4389491cab979b
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2023-12-29 17:42:39 -0600

    Variables en JavaScript

---
+++ IntroduccionJS/js/01.js
@@ -0,0 +1,22 @@
+// Variables
+
+var producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+var disponible; // Iniciamos la variable pero sin valor 
+
+producto = true; // Reasignamos el valor de la variable
+
+disponible = true;
+
+
+var producto1 = 'Computadora',
+    disponibilidad = true,
+    categoria = 'Computadoras';
+
+// Estilos para las variables
+var nombre_producto = 'Monitor HD'; // Underscore
+var nombreProducto = 'Monitor HD'; // Camelcase
+var NombreProducto = 'Monitor HD'; // Pascal Case
+var nombreproducto = 'Monitor HD';
+
+console.log(producto);
