0eb94d2884a19c06bba553cab3aae57afe028505
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2023-12-29 17:50:23 -0600

    Variables con let y const

---
+++ IntroduccionJS/js/02.js
@@ -0,0 +1,20 @@
+// Variables con let
+
+let producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+let disponible; // Iniciamos la variable pero sin valor 
+
+producto = true; // Reasignamos el valor de la variable
+
+disponible = true;
+
+
+let producto1 = 'Computadora',
+    disponibilidad = true,
+    categoria = 'Computadoras';
+
+// Estilos para las variables
+let nombre_producto = 'Monitor HD'; // Underscore
+let nombreProducto = 'Monitor HD'; // Camelcase
+let NombreProducto = 'Monitor HD'; // Pascal Case
+let nombreproducto = 'Monitor HD';
---
+++ IntroduccionJS/js/03.js
@@ -0,0 +1,20 @@
+// Variables con let
+
+const producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+const disponible; // No se puede dejar sin inicializar 
+
+producto = true; // Reasignamos el valor de la variable
+
+disponible = true;
+
+
+const producto1 = 'Computadora',
+    disponibilidad = true,
+    categoria = 'Computadoras';
+
+// Estilos para las variables
+const nombre_producto = 'Monitor HD'; // Underscore
+const nombreProducto = 'Monitor HD'; // Camelcase
+const NombreProducto = 'Monitor HD'; // Pascal Case
+const nombreproducto = 'Monitor HD';
