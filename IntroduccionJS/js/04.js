247784f690f8228e4e10036583a3f07797ed52c1
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2023-12-29 17:56:51 -0600

    Strings o Cadenas de Texto

---
+++ IntroduccionJS/js/04.js
@@ -0,0 +1,11 @@
+// Strings o Cadenas de Texto
+
+const producto = "Monitor de 20 Pulgadas";
+const producto2 = String('Monitor 30 Pulgadas');
+const producto3 = new String('Monitor 50 Pulgadas');
+
+const product = "Monitor de 20 Pulgadas\"" // Así podemos usar otra comilla doble sin marcar errores
+
+console.log(producto)
+console.log(producto2)
+console.log(producto3)
