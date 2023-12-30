4c8a17bc82484d815dfc7495553812db769dd2b7
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2023-12-29 18:40:01 -0600

    Controlar el Orden de las operaciones

---
+++ IntroduccionJS/js/07.js
@@ -0,0 +1,24 @@
+// Orden de las operaciones
+
+let resultado;
+
+resultado = (20 + 30) * 2;
+
+resultado = (100 + 200 + 300) * .2;
+
+// console.log(resultado);
+
+
+// Incrementos
+
+let puntaje = 10;
+
+// Incremento en 1 ++
+puntaje++; // Primero es el valor y después lo incrementa (10)
+++puntaje; // Primero incrementa y asigna el valor (12)
+
+puntaje--;
+
+puntaje += 10;
+
+console.log(puntaje);
