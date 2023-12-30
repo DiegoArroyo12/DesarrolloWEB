992935da7b3f985bc6abc038b110feac1b14b22a
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2023-12-29 18:19:32 -0600

    Números y Operadores

---
+++ IntroduccionJS/js/05.js
@@ -0,0 +1,15 @@
+// Números
+
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 200.20;
+const numero4 = .1020;
+const numero5 = -5;
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero1 + numero2); // Suma
+console.log(numero2 - numero1); // Resta
+console.log(numero2 * numero1); // Multiplicación
+console.log(numero2 / numero1); // División
+console.log(numero2 % numero1); // Módulo (Residuo de la División)
