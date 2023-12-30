02a77fe848c5e6ee98e2bf01a3feb52f585a0c12
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2023-12-29 20:23:49 -0600

    Booleanos

---
+++ IntroduccionJS/js/09.js
@@ -0,0 +1,10 @@
+// Booleanos
+
+const booleano1 = true;
+const booleano2 = false;
+
+console.log(booleano1);
+console.log(booleano2);
+
+const booleano3 = new Boolean(true);
+console.log(booleano3);
