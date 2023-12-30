9219039293b1002a13865a78228250491d70e852
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2023-12-29 20:13:24 -0600

    Concatenación y Template Strings

---
+++ IntroduccionJS/js/08.js
@@ -0,0 +1,9 @@
+const nombre = 'Diego';
+const email = 'correo@correo.com';
+
+// Concatenación
+console.log("Nombre CLiente: " + nombre + "Email: " + email);
+
+
+// Template Strings - String Literals
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
