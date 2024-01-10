faaffc52637c9cddfc15dac17bfe5bebe1fee479
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2024-01-09 22:56:19 -0600

    While y Do While

--- IntroduccionJS/js/26.js
+++ IntroduccionJS/js/26.js
@@ -23,7 +23,33 @@ for(let i = 0; i < carrito.length; i++) {
     console.log(carrito[i].nombre);
 }
 
-// While loop
+// While Loop
+let i = 0; // Indice
 
+while(i <= 100) { // Condición
+
+    if(i % 2 === 0) {
+        console.log(`El número ${i} es PAR)`);
+    } else {
+        console.log(`El número ${i} es PAR)`);
+    }
+
+    i++; // Incremento
+}
+
+let j = 0;
+
+while (j < carrito.length) {
+    console.log(carrito[j].nombre);
+    j++;
+}
 
 // Do While Loop
+
+let t = 0;
+
+// El código se ejecuta una vez y después evalúa
+do {
+    console.log(i);
+    i++;
+} while (t < 10);
