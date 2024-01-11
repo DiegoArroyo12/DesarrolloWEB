60d05ff407cf7939b573db3b7c509d99f9170e2e
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2024-01-10 19:48:49 -0600

    Herencia

--- IntroduccionJS/js/30.js
+++ IntroduccionJS/js/30.js
@@ -9,6 +9,10 @@ class Producto {
     formatearProducto() {
         return `El Producto ${this.nombre} tiene un precio de: ${this.precio}`
     }
+
+    obtenerPrecio() {
+        console.log(this.precio);
+    }
 };
 
 const producto = new Producto('Monitor Curvo de 49"', 800);
@@ -16,3 +20,25 @@ const producto2 = new Producto('Laptop', 300);
 
 console.log(producto);
 console.log(producto2.formatearProducto());
+
+//Herencia
+class Libro extends Producto {
+    constructor(nombre, precio, isbn) {
+        super(nombre, precio); // Heredamos el constructor
+        this.isbn = isbn;
+    }
+
+    formatearProducto() {
+        return `${super.formatearProducto()} y su isbn es ${this.isbn}`
+    }
+
+    obtenerPrecio() {
+        super.obtenerPrecio();
+        console.log('Y si hay en existencia');
+    }
+};
+
+const libro = new Libro('JavaScript la Revolución', 120, '541651513218754987982');
+console.log(libro);
+console.log(libro.formatearProducto());
+console.log(libro.obtenerPrecio());
