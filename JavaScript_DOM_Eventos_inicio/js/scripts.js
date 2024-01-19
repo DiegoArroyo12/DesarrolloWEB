f112c8b54deebd8ae1a7e32d0a564f14c3f676d1
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2024-01-19 15:32:41 -0600

    Seleccionar Elementos con querySelectorAll

--- JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -6,6 +6,14 @@ console.log(heading);
 
 // querySelectorAll
 
-
+const enlaces = document.querySelectorAll('.navegacion a');
+enlaces[0].textContent = 'Nuevo Texto Para Enlace';
+enlaces[0].href = 'https://google.com';
+enlaces[0].classList.add('nueva-clase');
+enlaces[0].classList.remove('navegacion__enlace');
+console.log(enlaces[0]);
 
 // getElementById
+
+const heading2 = document.getElementById("heading");
+console.log(heading2);
