cded339dbcfbba7912b419cd005ecd67fbd98145
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2024-01-19 14:57:36 -0600

    Seleccionar Elementos con querySelector

---
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -0,0 +1,11 @@
+// querySelector
+
+const heading = document.querySelector(".header__texto h2"); // 0 o 1 Elementos
+heading.textContent = 'Nuevo Heading'
+console.log(heading);
+
+// querySelectorAll
+
+
+
+// getElementById
