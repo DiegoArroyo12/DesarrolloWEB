ae166d5b60e3b7edf23c14e6d3747ded7723eeaf
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2024-02-03 22:12:30 -0600

    Eventos en JavaScript

--- JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -34,3 +34,26 @@ const navegacion = document.querySelector('.navegacion');
 navegacion.appendChild(nuevoEnlace);
 
 console.log(nuevoEnlace);
+
+// Eventos
+
+console.log(1);
+
+window.onload = function () {
+    console.log(3);
+};
+
+// window hace referencia todo el contenido de la página, document hace más referencia al HTML
+window.addEventListener('load', function () {
+    console.log(2);
+}); // load espera a que el JS y los archivos que dependen del HTML estén listos
+
+document.addEventListener('DOMContentLoaded', function () { // Solo espera que se descargue el HTML, pero no espera al CSS o las imágenes
+    console.log(4);
+});
+
+console.log(5);
+
+window.onscroll = function () {
+    console.log('scrolling...'); // Detecta si se hace scroll
+};
