c63b0161e823a7dd33ca7259602e7e064f6bda21
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2024-02-15 10:54:38 -0600

    Añadiendo Plumber

--- FestivalMusica_inicio/gulpfile.js
+++ FestivalMusica_inicio/gulpfile.js
@@ -1,8 +1,10 @@
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const plumber = require('gulp-plumber');
 
 function css(done) {
     src("src/scss/**/*.scss") // Identificar el archivo SASS
+        .pipe(plumber())
         .pipe(sass())     // Compilarlo
         .pipe(dest("build/css") );     // Almacenarla en el disco duro
 
