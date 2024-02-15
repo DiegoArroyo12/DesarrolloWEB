67dc6b9deaed9d9a8f30196e8ad59abac3674833
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2024-02-15 10:43:02 -0600

    Compilar todos los archivos SCSS

--- FestivalMusica_inicio/gulpfile.js
+++ FestivalMusica_inicio/gulpfile.js
@@ -2,7 +2,7 @@ const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function css(done) {
-    src("src/scss/app.scss") // Identificar el archivo SASS
+    src("src/scss/**/*.scss") // Identificar el archivo SASS
         .pipe(sass())     // Compilarlo
         .pipe(dest("build/css") );     // Almacenarla en el disco duro
 
@@ -10,7 +10,7 @@ function css(done) {
 }
 
 function dev(done) {
-    watch("src/scss/app.scss", css)
+    watch("src/scss/**/*.scss", css)
 
     done();
 }
