80ac3f2650c53e9a2b29df09499b5570125f40a7
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2024-03-18 23:17:22 -0600

    Utilizando Gulp para crear imágenes Webp

--- FestivalMusica_inicio/gulpfile.js
+++ FestivalMusica_inicio/gulpfile.js
@@ -1,6 +1,11 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel} = require("gulp");
+
+// CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require('gulp-plumber');
+
+// Imagenes
+const webp = require('gulp-webp');
 
 function css(done) {
     src("src/scss/**/*.scss") // Identificar el archivo SASS
@@ -11,6 +16,19 @@ function css(done) {
     done(); // Callback que avisa a gulp cuando llegamos al final
 }
 
+function versionWebp( done ) {
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}') // Entre llaves busca esos dos tipos de formatos
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') )
+    
+    done();
+}
+
 function dev(done) {
     watch("src/scss/**/*.scss", css)
 
@@ -18,7 +36,8 @@ function dev(done) {
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp = versionWebp;
+exports.dev = parallel( versionWebp, dev ); // npm i -D gulp-webp@4
 
 // npx gulp 'nombre de la tarea'
 // npm run 'nombre del package de json'
