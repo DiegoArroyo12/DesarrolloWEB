0b18fad9265c9765c34d3239c19ca6653cb0b3fd
Diego Arroyo Palacios <diego.arroyo.pal@gmail.com>
2024-03-20 21:12:28 -0600

    Creando imágenes Avif

--- FestivalMusica_inicio/gulpfile.js
+++ FestivalMusica_inicio/gulpfile.js
@@ -8,6 +8,7 @@ const plumber = require('gulp-plumber');
 const cache = require('gulp-cache');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done) {
     src("src/scss/**/*.scss") // Identificar el archivo SASS
@@ -42,6 +43,19 @@ function versionWebp( done ) {
     done();
 }
 
+function versionAvif( done ) {
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}') // Entre llaves busca esos dos tipos de formatos
+        .pipe( avif(opciones) )
+        .pipe( dest('build/img') )
+    
+    done();
+}
+
 function dev(done) {
     watch("src/scss/**/*.scss", css)
 
@@ -51,7 +65,8 @@ function dev(done) {
 exports.css = css;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel( imagenes, versionWebp, dev ); // npm i -D gulp-webp@4
+exports.versionWebp = versionAvif;
+exports.dev = parallel( imagenes, versionWebp, versionAvif, dev ); // npm i -D gulp-webp@4
 
 // npx gulp 'nombre de la tarea'
 // npm run 'nombre del package de json'
